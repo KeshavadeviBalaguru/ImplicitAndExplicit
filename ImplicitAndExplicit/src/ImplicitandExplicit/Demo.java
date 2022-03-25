6f26dc8d337aa7ba5ed215723eff84f093df33ec
ELCOT <ELCOT@Lenovo>
2022-03-25 20:09:29 +0530

    first commit

---
+++ ImplicitAndExplicit/src/ImplicitandExplicit/Demo.java
@@ -0,0 +1,14 @@
+package ImplicitandExplicit;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	float ans;
+	int i=7;
+	int j=2;
+	ans=(float)i/j;
+	System.out.println("ans is "+ans);
+
+	}
+
+}
